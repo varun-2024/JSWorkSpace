3f14a4306ff448639c0f94410311065c57ce62d5
varun-2024 <v.talwar2024@gmail.com>
2025-02-08 22:54:11 +0000

    Changes

---
+++ practice5.js
@@ -0,0 +1,10 @@
+// Math Methods
+
+console.log(Math.PI); // 3.141592653589793
+console.log(Math.SQRT2);
+console.log(Math.E); // 2.718281828459045
+console.log(Math.abs(-986)); // 2.7182818284590
+console.log(Math.pow(2, 7)); // 2.7182818284590
+console.log(Math.floor(5.9874)); // 2.718281828459045
+console.log(Math.ceil(5.9874)); // 
+console.log(Math.floor(Math.random() * 10 + 1)); // Random number between 0 and 100
